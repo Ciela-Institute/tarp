6647105de451bdc78650534e35097dbdf72ffcb8
Pablo-Lemos <plemos91@gmail.com>
2023-11-21 09:50:41 -0500

    Reformatting

--- src/tarp/drp.py
+++ src/tarp/drp.py
@@ -1,115 +1,38 @@
 from typing import Tuple, Union
-
 import numpy as np
 from tqdm import tqdm
+from warnings import deprecated
 
 
 __all__ = ("get_drp_coverage",)
 
 
-
-def old_get_drp_coverage(
-    samples: np.ndarray,
-    theta: np.ndarray,
-    references: Union[str, np.ndarray] = "random",
-    metric: str = "euclidean",
-    seed: Union[int, None] = None,
-    ) -> Tuple[np.ndarray, np.ndarray]:
-    """
-    Estimates coverage with the distance to random point method.
-
-    Reference: `Lemos, Coogan et al 2023 <https://arxiv.org/abs/2302.03026>`_
-
-    Args:
-        samples: the samples to compute the coverage of, with shape ``(n_samples, n_sims, n_dims)``.
-        theta: the true parameter values for each samples, with shape ``(n_sims, n_dims)``.
-        references: the reference points to use for the DRP regions, with shape
-            ``(n_references, n_sims)``, or the string ``"random"``. If the later, then
-            the reference points are chosen randomly from the unit hypercube over
-            the parameter space.
-        metric: the metric to use when computing the distance. Can be ``"euclidean"`` or
-            ``"manhattan"``.
-
-    Returns:
-        Credibility values (``alpha``) and expected coverage probability (``ecp``).
-    """
-
-    np.random.seed(seed)
-
-    # Check that shapes are correct
-    if samples.ndim != 3:
-        raise ValueError("samples must be a 3D array")
-
-    if theta.ndim != 2:
-        raise ValueError("theta must be a 2D array")
-
-    num_samples = samples.shape[0]
-    num_sims = samples.shape[1]
-    num_dims = samples.shape[2]
-
-    if theta.shape[0] != num_sims:
-        raise ValueError("theta must have the same number of rows as samples")
-
-    if theta.shape[1] != num_dims:
-        raise ValueError("theta must have the same number of columns as samples")
-
-    # Reshape theta
-    theta = theta[np.newaxis, :, :]
-
-    # Normalize
-    low = np.min(theta, axis=1, keepdims=True)
-    high = np.max(theta, axis=1, keepdims=True)
-    samples = (samples - low) / (high - low + 1e-10)
-    theta = (theta - low) / (high - low + 1e-10)
-
-    # Generate reference points
-    if isinstance(references, str) and references == "random":
-        references = np.random.uniform(low=0, high=1, size=(num_sims, num_dims))
-    else:
-        assert isinstance(references, np.ndarray)  # to quiet pyright
-        if references.ndim != 2:
-            raise ValueError("references must be a 2D array")
-
-        if references.shape[0] != num_sims:
-            raise ValueError("references must have the same number of rows as samples")
-
-        if references.shape[1] != num_dims:
-            raise ValueError(
-                "references must have the same number of columns as samples"
-            )
-
-    # Compute distances
-    if metric == "euclidean":
-        samples_distances = np.sqrt(
-            np.sum((references[np.newaxis] - samples) ** 2, axis=-1)
-        )
-        theta_distances = np.sqrt(np.sum((references - theta) ** 2, axis=-1))
-    elif metric == "manhattan":
-        samples_distances = np.sum(np.abs(references[np.newaxis] - samples), axis=-1)
-        theta_distances = np.sum(np.abs(references - theta), axis=-1)
-    else:
-        raise ValueError("metric must be either 'euclidean' or 'manhattan'")
-
-    # Compute coverage
-    f = np.sum((samples_distances < theta_distances), axis=0) / num_samples
-
-    # Compute expected coverage
-    h, alpha = np.histogram(f, density=True, bins=num_sims // 10)
-    dx = alpha[1] - alpha[0]
-    ecp = np.cumsum(h) * dx
-    return ecp, alpha[1:]
-
-
+@deprecated("get_drp_coverage", "get_tarp_coverage")
 def get_drp_coverage(
     samples: np.ndarray,
     theta: np.ndarray,
     references: Union[str, np.ndarray] = "random",
     metric: str = "euclidean",
-    norm : bool = True,
+) -> Tuple[np.ndarray, np.ndarray]:
+    return get_tarp_coverage(samples=samples,
+                             theta=theta,
+                             references=references,
+                             metric=metric,
+                             norm=True,
+                             bootstrap=False,
+                             seed=None)
+
+
+def _get_tarp_coverage_single(
+    samples: np.ndarray,
+    theta: np.ndarray,
+    references: Union[str, np.ndarray] = "random",
+    metric: str = "euclidean",
+    norm: bool = True,
     seed: Union[int, None] = None
-    ) -> Tuple[np.ndarray, np.ndarray]:
+) -> Tuple[np.ndarray, np.ndarray]:
     """
-    Estimates coverage with the distance to random point method.
+    Estimates coverage with the TARP method a single time.
 
     Reference: `Lemos, Coogan et al 2023 <https://arxiv.org/abs/2302.03026>`_
 
@@ -123,6 +46,7 @@ def get_drp_coverage(
         metric: the metric to use when computing the distance. Can be ``"euclidean"`` or
             ``"manhattan"``.
         norm : whether to apply or not the normalization (Default = True)
+        seed: the seed to use for the random number generator. If ``None``, then no seed
 
     Returns:
         Expected coverage probability (``ecp``) and credibility values (``alpha``) 
@@ -150,7 +74,7 @@ def get_drp_coverage(
     theta = theta[np.newaxis, :, :]
 
     # Normalize
-    if norm :
+    if norm:
         low = np.min(theta, axis=1, keepdims=True)
         high = np.max(theta, axis=1, keepdims=True)
         samples = (samples - low) / (high - low + 1e-10)
@@ -193,10 +117,17 @@ def get_drp_coverage(
     ecp = np.cumsum(h) * dx
     return ecp, alpha[1:]
 
-def bootstrapping(samples, theta, references = "random", metric = "euclidean", norm = True):
+
+def _get_tarp_coverage_bootstrap(samples: np.ndarray,
+    theta: np.ndarray,
+    references: Union[str, np.ndarray] = "random",
+    metric: str = "euclidean",
+    norm: bool = True,
+    seed: Union[int, None] = None
+) -> Tuple[np.ndarray, np.ndarray]:
     """
-    Estimates uncertainties on the expected probability and credibility values calculated with the get_drp_coverage function
-    using the bootstrapping method
+    Estimates uncertainties on the expected probability and credibility values calculated with the
+    _get_tarp_coverage_single function using the bootstrapping method
 
     Args:
         samples: the samples to compute the coverage of, with shape ``(n_samples, n_sims, n_dims)``.
@@ -208,17 +139,16 @@ def bootstrapping(samples, theta, references = "random", metric = "euclidean", n
         metric: the metric to use when computing the distance. Can be ``"euclidean"`` or
             ``"manhattan"``.
         norm : whether to apply or not the normalization (Default = True)
+        seed: the seed to use for the random number generator. If ``None``, then no seed
 
     Returns:
         Mean and std of the expected coverage probability, and mean of the credibility
     """
-    num_samples = samples.shape[0]
     num_sims = samples.shape[1]
-    num_dims = samples.shape[2]
 
-    boot_ecp = np.empty(shape = (num_sims, num_sims//10))
-    boot_alpha = np.empty(shape = (num_sims, num_sims//10))
-    for i in tqdm(range(num_sims)): 
+    boot_ecp = np.empty(shape=(num_sims, num_sims//10))
+    boot_alpha = np.empty(shape=(num_sims, num_sims//10))
+    for i in tqdm(range(num_sims)):
         idx_remove = np.random.randint(num_sims)
         idx_add = np.random.randint(num_sims)
 
@@ -226,9 +156,54 @@ def bootstrapping(samples, theta, references = "random", metric = "euclidean", n
         samples[:, idx_remove, :] = samples[:, idx_add, :] 
         theta[idx_remove, :] = theta[idx_add, :]
 
-        boot_ecp[i, :], boot_alpha[i, :] = get_drp_coverage(samples, theta, references = references, metric = metric, norm = norm)
+        boot_ecp[i, :], boot_alpha[i, :] = _get_tarp_coverage_single(samples,
+                                                                     theta,
+                                                                     references=references,
+                                                                     metric=metric,
+                                                                     norm=norm,
+                                                                     seed=seed)
     
-    ecp_mean = boot_ecp.mean(axis = 0)
-    ecp_std = boot_ecp.std(axis = 0)
-    alpha_mean = boot_alpha.mean(axis = 0)
-    return ecp_mean, ecp_std, alpha_mean
+    # ecp_mean = boot_ecp.mean(axis=0)
+    # ecp_std = boot_ecp.std(axis=0)
+    # alpha_mean = boot_alpha.mean(axis=0)
+    # return ecp_mean, ecp_std, alpha_mean
+    return boot_ecp, boot_alpha
+
+
+def get_tarp_coverage(
+    samples: np.ndarray,
+    theta: np.ndarray,
+    references: Union[str, np.ndarray] = "random",
+    metric: str = "euclidean",
+    norm: bool = False,
+    bootstrap: bool = False,
+    seed: Union[int, None] = None
+) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Estimates coverage with the TARP method.
+
+    Reference: `Lemos, Coogan et al 2023 <https://arxiv.org/abs/2302.03026>`_
+
+    Args:
+        samples: the samples to compute the coverage of, with shape ``(n_samples, n_sims, n_dims)``.
+        theta: the true parameter values for each samples, with shape ``(n_sims, n_dims)``.
+        references: the reference points to use for the DRP regions, with shape
+            ``(n_references, n_sims)``, or the string ``"random"``. If the later, then
+            the reference points are chosen randomly from the unit hypercube over
+            the parameter space.
+        metric: the metric to use when computing the distance. Can be ``"euclidean"`` or
+            ``"manhattan"``.
+        norm : whether to apply or not the normalization (Default = False)
+        bootstrap : whether to use bootstrap to estimate uncertainties (Default = False)
+        seed: the seed to use for the random number generator. If ``None``, then no seed
+
+    Returns:
+        Expected coverage probability (``ecp``) and credibility values (``alpha``).
+        If bootstrap is True, both arrays have an extra dimension corresponding to the number of bootstrap iterations
+    """
+    if bootstrap:
+        ecp, alpha = _get_tarp_coverage_bootstrap(samples, theta, references, metric, norm, seed)
+    else:
+        ecp, alpha = _get_tarp_coverage_single(samples, theta, references, metric, norm, seed)
+    return ecp, alpha
+
